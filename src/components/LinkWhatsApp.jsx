5d4767fa54d428d410754e3e72440f1238494260
Adson Nascimento <adson.nascimento.112@ufrn.edu.br>
2023-04-27 08:46:10 -0300

    finesh page whatsapp

--- src/components/LinkWhatsApp.jsx
+++ src/components/LinkWhatsApp.jsx
@@ -1,7 +1,7 @@
 import { Check } from "@phosphor-icons/react";
 import { useState } from "react";
 
-export default function LinkWhatsApp({ telNumber, mensage }) {
+export default function LinkWhatsApp({ country, telNumber, mensage }) {
 
   function htmlDecode(text) {
     var doc = new DOMParser().parseFromString(text, "text/html");
@@ -25,38 +25,27 @@ export default function LinkWhatsApp({ telNumber, mensage }) {
     document.body.removeChild(textarea);
   }
 
-  const [btn, setBtn] = useState([
-    <span>copy mensage</span>
-  ])
+  const [btn, setBtn] = useState('copy mensage')
 
-  // function alterBtn() {
-  //   const stateBtn = true;
+  function ok() {
+    setBtn([
+      <Check size={16} color="#000" />,
+      <span>copyed!</span>
+    ])
 
-  //   !stateBtn
-
-  //   setTimeout(() => {
-  //     if (stateBtn) {
-  //       setBtn([
-  //         <span>copy mensage</span>
-  //       ])
-  //     } else {
-  //       !stateBtn
-
-  //       setBtn([
-  //         <Check size={16} color="#000" />,
-  //         <span>copyed!</span>
-  //       ])
-  //     }
-  //   }, 1000)
-  // }
-  
+    setTimeout(() => {
+      setBtn('copy mensage')
+    }, 1250)
+  }
 
   function handleLink() {
     const formattedMensage = htmlDecode(mensage).trim().replace(/\s/g, '+');
-    const url = `https://api.whatsapp.com/send/?phone=${telNumber}&text=${formattedMensage}&app_absent=0`;
-    copyToClipboard(url)
+    const url = `https://api.whatsapp.com/send/?phone=${country}${telNumber}&text=${formattedMensage}&app_absent=0`;
 
-    // alterBtn()
+    if (telNumber, mensage) {
+      copyToClipboard(url)
+      ok()
+    }
   }
 
   return (
--- src/pages/whatsapp/index.jsx
+++ src/pages/whatsapp/index.jsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 export default function WhatsAppPage() {
   const [number, setNumber] = useState('')
   const [mensage, setMesage] = useState('')
+  const [country, setCountry] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -14,8 +15,19 @@ export default function WhatsAppPage() {
     const onlyNumbers = event.target.value.replace(/[^0-9]/g, '');
     const key = event.keyCode || event.charCode;
 
-    if (onlyNumbers === event.target.value || key === 8 || key === 8) {
+    if (onlyNumbers === event.target.value || key === 8 || key === 9) {
       setNumber(onlyNumbers);
+    } else {
+      alert('Enter only numbers!');
+    }
+  }
+
+  const handleCountry = (event) => {
+    const onlyNumbers = event.target.value.replace(/[^0-9]/g, '');
+    const key = event.keyCode || event.charCode;
+
+    if (onlyNumbers === event.target.value || key === 8 || key === 9) {
+      setCountry(onlyNumbers);
     } else {
       alert('Enter only numbers!');
     }
@@ -30,18 +42,33 @@ export default function WhatsAppPage() {
       <form className='w-full' onSubmit={handleSubmit}>
 
         <label className='w-full' htmlFor="number">Phone number:</label><br />
-        <input
-          className='w-full h-12 rounded-xl p-4 mb-9'
-          value={number}
-          onChange={handleNumber}
-          id="number"
-          name="number"
-          type="tel"
-          autoComplete='off'
-          maxLength="15"
-          placeholder="Only numbers"
-          required
-        />
+        <fieldset className="flex gap-2">
+          <input
+            className='w-1/4 h-12 rounded-xl p-4 mb-9'
+            value={country}
+            onChange={handleCountry}
+            id="Country"
+            name="Country"
+            type="tel"
+            autoComplete='off'
+            maxLength="3"
+            placeholder="Country"
+            required
+          />
+
+          <input
+            className='w-3/4 h-12 rounded-xl p-4 mb-9'
+            value={number}
+            onChange={handleNumber}
+            id="number"
+            name="number"
+            type="tel"
+            autoComplete='off'
+            maxLength="15"
+            placeholder="Only numbers"
+            required
+          />
+        </fieldset>
 
         <label className='w-full' htmlFor="mensage">Write the text:</label><br />
         <textarea
@@ -55,7 +82,7 @@ export default function WhatsAppPage() {
           required>
         </textarea>
 
-        <LinkWhatsApp telNumber={number} mensage={mensage} />
+        <LinkWhatsApp country={country} telNumber={number} mensage={mensage} />
       </form>
     </main>
   )
