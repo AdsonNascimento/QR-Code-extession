9a56bfc098d73e6c4037d36c84a8eeb8d463704a
Adson Nascimento <adson.nascimento.112@ufrn.edu.br>
2023-04-27 09:56:00 -0300

    backspace

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      width: {
+        '128': '40rem',
+      }
     },
   },
   plugins: [],
